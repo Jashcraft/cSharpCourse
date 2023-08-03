f95d89ac5a87662e64eb014596cdeb5db569bbbe
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-02 20:51:28 -0600

    changed course

---
+++ Section 4/MethodsTesting/MethodsTesting/Methods/DisplayMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsTesting.Methods
+{
+  internal class DisplayMethod
+  {
+    public static void DisplayMessage()
+    {
+      Console.WriteLine("This is the message");
+      Console.Read();
+    }
+  }
+}
---
+++ Section 4/MethodsTesting/MethodsTesting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Practice
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      AskName();
+
+    }
+
+
+
+
+
+    public static void AskName()
+    {
+
+      Console.WriteLine("What is your name?");
+      string name = Console.ReadLine();
+      Console.WriteLine("is your name " + name + "?");
+      Console.WriteLine("Press Enter to Exit!");
+      Console.Read();
+    }
+
+
+
+    public static int Minus(int num1, int num2)
+    {
+      int value = num1 - num2;
+      return value;
+    }
+
+  }
+}
