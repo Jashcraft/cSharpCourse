53f1cdeb7c738fb168c11edeee5ef36c300660fa
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-14 11:03:17 -0600

    added divide by zero

--- Section 4/MethodsTesting/TryCatchFinally/Program.cs
+++ Section 4/MethodsTesting/TryCatchFinally/Program.cs
@@ -12,7 +12,7 @@ namespace TryCatchFinally
 
       try
       {
-          //some methods will tell you the possible exceptions when you hover over them.
+        //some methods will tell you the possible exceptions when you hover over them.
         int userInputAsInt = int.Parse(userInput);
       }
       catch (FormatException)
@@ -24,17 +24,35 @@ namespace TryCatchFinally
       {
         Console.WriteLine("OverFlow Exception: Number to was too small or too large for an INT 32");
       }
-      catch(ArgumentNullException) 
+      catch (ArgumentNullException)
       {
         Console.WriteLine("ArgumentNulllException: Value was Null/Empty");
-      }finally 
+      }
+      finally
       {
         Console.WriteLine("This is called anyways...");
       };
 
 
-
+      divideByZero();
       Console.ReadKey();
     }
+
+    public static void divideByZero()
+    {
+      Console.WriteLine("Please Choose A Number for division:");
+      string input1 = Console.ReadLine();
+      Console.WriteLine("Please Choose another number for division:");
+      string input2 = Console.ReadLine();
+
+      int div1 = int.Parse(input1);
+      int div2 = int.Parse(input2);
+
+      int division = div1 / div2;
+
+      Console.WriteLine(division);
+
+    }
+
   }
 }
