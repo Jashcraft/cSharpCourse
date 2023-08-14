c32c9f527b7f38595baf1cf1bff1cccbc84dd9d8
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-14 14:16:09 -0600

    added enhanced ifs

---
+++ Making Decisions/MakingDecisions/EnhancedIfStatement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace EnhancedIf
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int temperature = -5;
+			string stateOfMatter;
+
+			if (temperature < 32)
+			{
+				stateOfMatter = "Solid";
+			}
+			else
+			{
+				stateOfMatter = "Liquid";
+			}
+
+			Console.WriteLine("State of matter is {0}", stateOfMatter);
+			Console.Read();
+
+
+
+		}
+
+    public static void enhancedIf()
+    {
+			string stateOfWater;
+			int temp = 12;
+			stateOfWater = temp < 32 ? "Solid" : "Liquid";
+    }
+  }
+}
