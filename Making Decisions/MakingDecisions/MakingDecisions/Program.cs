185e820bcfd575c6c5187fb3249d627899cb21fd
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-14 12:28:35 -0600

    added parseTry logic

--- Making Decisions/MakingDecisions/MakingDecisions/Program.cs
+++ Making Decisions/MakingDecisions/MakingDecisions/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
 
 namespace HelloWorld
 {
@@ -6,34 +8,67 @@ namespace HelloWorld
 	{
 		static void Main(string[] args)
 		{
-			int temperature = 60;
+      //judgeTemperature();
+      judgeTempRefactor();
 
 
-			Console.WriteLine("What is the temperature outside ?:");
-
-			string userInput = Console.ReadLine();
-
-			temperature = int.Parse(userInput);
-			if (temperature < 50)
-			{
-				Console.WriteLine("It is chilly outside");
-			}
-			if (temperature < 80 && temperature > 60)
-			{
-				Console.WriteLine("It's perfect outside!");
-			}
-			if (temperature < 90 && temperature > 80)
-			{
-				Console.WriteLine("It's warm outside");
-			}
-			if (temperature > 90)
-			{
-				Console.WriteLine("It is too hot outside");
-			}
-
-
-
+      Console.WriteLine("Please press ENTER to exit:");
 			Console.Read();
-		}
-	}
+    }
+
+    public static void judgeTemperature()
+    {
+      int temperature = 60;
+
+
+      Console.WriteLine("What is the temperature outside ?:");
+
+      string userInput = Console.ReadLine();
+
+      temperature = int.Parse(userInput);
+      if (temperature < 50)
+      {
+        Console.WriteLine("It is chilly outside");
+      }
+      if (temperature < 80 && temperature > 60)
+      {
+        Console.WriteLine("It's perfect outside!");
+      }
+      if (temperature < 90 && temperature > 80)
+      {
+        Console.WriteLine("It's warm outside");
+      }
+      if (temperature > 90)
+      {
+        Console.WriteLine("It is too hot outside");
+      }
+    }
+    public static void judgeTempRefactor()
+    {
+      int temp = 0;
+
+      Console.WriteLine("What is the temperature outside ?: ");
+      string userInput = Console.ReadLine();
+      temp = int.Parse(userInput);
+
+      if (temp < 30)
+      {
+        Console.WriteLine("It's Freezing! Go play outside!");
+
+      } else if (temp < 60)
+      {
+        Console.WriteLine("It's Chilly Outside!");
+      } else if (temp < 80)
+      {
+        Console.WriteLine("It's perfect outside! Take the day off work!");
+      } else if (temp < 90)
+      {
+        Console.WriteLine("It's warm outside be carfeul!");
+      } else
+      {
+        Console.WriteLine("It's too hot outside");
+      }
+
+    }
+  }
 }
---
+++ Making Decisions/MakingDecisions/TryParse/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace TryParse
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      init();
+
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.WriteLine("\nProgram Complete! Closing automatically in 5 seconds...");
+      System.Threading.Thread.Sleep(5000);
+    }
+
+    public static void parseExample()
+    {
+      Console.WriteLine("Choose a number to parse:");
+      string numberAsString = Console.ReadLine();
+
+
+      int parsedValue;
+      bool success = int.TryParse(numberAsString, out parsedValue);
+      if (success)
+      {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nParsing Successful! Number saved is: {0}", parsedValue);
+      }
+      else
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Parsing FAILED: Check input data below:\n");
+        Console.WriteLine("'" + numberAsString + "'");
+      }
+
+    }
+
+    public static void init()
+    {
+      Console.WriteLine("Enable floating point (decimals) (y/n)?");
+      string userInput = Console.ReadLine();
+      bool floatingEnabled = false;
+
+      if (userInput == "y")
+      {
+        floatingEnabled = true;
+        Console.WriteLine("\nFloating Enabled!");
+      }
+      else if (userInput == "n")
+      {
+        Console.WriteLine("\nFloating Disabled!");
+        floatingEnabled = false;
+      }
+      else
+      {
+        Console.WriteLine("\nOption not understood, Floating Disabled.");
+        floatingEnabled = false;
+      }
+
+      if (floatingEnabled)
+      {
+        parseFloatingEnabled();
+      }
+      else
+      {
+        parseExample();
+      }
+      
+    }
+
+    public static void parseFloatingEnabled()
+    {
+      Console.WriteLine("Choose a number to parse (decimals allowed):");
+      string numberAsString = Console.ReadLine();
+
+
+      float parsedValue;
+      bool success = float.TryParse(numberAsString, out parsedValue);
+      if (success)
+      {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nParsing Successful! Number saved is: {0}", parsedValue);
+      }
+      else
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Parsing FAILED: Check input data below:\n");
+        Console.WriteLine("'" + numberAsString + "'");
+      }
+    }
+
+  }
+}
