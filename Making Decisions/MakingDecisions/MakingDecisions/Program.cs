c6ea2453ed852407a1892ee097abfa521c9afe83
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-14 11:39:01 -0600

    init MakingDecisions

---
+++ Making Decisions/MakingDecisions/MakingDecisions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int temperature = 60;
+
+
+			Console.WriteLine("What is the temperature outside ?:");
+
+			string userInput = Console.ReadLine();
+
+			temperature = int.Parse(userInput);
+			if (temperature < 50)
+			{
+				Console.WriteLine("It is chilly outside");
+			}
+			if (temperature < 80 && temperature > 60)
+			{
+				Console.WriteLine("It's perfect outside!");
+			}
+			if (temperature < 90 && temperature > 80)
+			{
+				Console.WriteLine("It's warm outside");
+			}
+			if (temperature > 90)
+			{
+				Console.WriteLine("It is too hot outside");
+			}
+
+
+
+			Console.Read();
+		}
+	}
+}
