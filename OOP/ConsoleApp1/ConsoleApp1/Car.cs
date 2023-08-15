ac8f54b0fb528475a279bd67782e3e3e9ed70e77
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-15 11:27:19 -0600

    OOP Added

---
+++ OOP/ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+  internal class Car
+  {
+    //Constructor
+    public Car()
+    {
+      Console.WriteLine("Car was created!");
+    }
+    //Methods on class.
+    public void Drive()
+    {
+      Console.WriteLine("Car is driving!");
+    }
+    public void Stop()
+    {
+      Console.WriteLine("Car has stopped!");
+    }
+  }
+}
---
+++ OOP/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using ClassesAndObjects;
+
+
+namespace ClassesAndObjects
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Car audi = new Car();
+      Car bmw = new Car();
+      audi.Drive();
+
+
+
+      Console.WriteLine("Press 1 to stop car.");
+      string userInput = Console.ReadLine();
+      if (userInput == "1")
+      {
+        audi.Stop();
+      }
+      else
+      {
+        Console.WriteLine("Car wrecks!");
+      }
+
+
+
+      Exit();
+
+    }
+
+    public static void Exit()
+    {
+      Console.WriteLine("Program will exit in 5 seconds...");
+      System.Threading.Thread.Sleep(5000);
+    }
+  }
+}
