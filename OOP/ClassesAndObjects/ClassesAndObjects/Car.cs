3c48474d5d2a03e2abd4b2259e7cc89a5391d769
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-15 12:45:48 -0600

    accidentally broke classes proj. made new

---
+++ OOP/ClassesAndObjects/ClassesAndObjects/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+  internal class Car
+  {
+    #region Member Variables
+    //Member variables
+    private string _name; //private field typically used for storing data. 
+    private int _hp;
+    private string _status;
+    private string _color;
+    private int _year;
+    #endregion
+
+    #region Constructors
+    //Default Constructor
+    public Car()
+    {
+      _name = "Car";
+      _hp = 0;
+      _year = 1999;
+      _color = "Black";
+      _status = "stopped";
+    }
+
+    //Partial Specific Constructor
+    public Car(string name, int hp)
+
+    {
+      Console.WriteLine(name + " was created with " + hp + " horsepower!");
+      _name = name;
+      _hp = hp;
+      _status = "Stopped";
+      _color = "Black";
+      _year = 1999;
+    }
+
+    // Specific Constructor
+
+    public Car(string name, string color, int hp, int year)
+    {
+      _name = name;
+      _color = color;
+      _hp = hp;
+      _year = year;
+      _status = "stopped";
+    }
+    #endregion
+
+    #region Member Methods
+    public void Drive()
+    {
+      Console.WriteLine(_name + " is driving!");
+      _status = "driving";
+    }
+    public void Stop()
+    {
+      Console.WriteLine(_name + " has stopped!");
+      _status = "stopped";
+    }
+
+    public void PrintStats()
+    {
+      Console.WriteLine("\n***Vehicle Stats***\n");
+      Console.WriteLine("   Vehicle Name: " + _name);
+      Console.WriteLine("   Vehicle Color: " + _color);
+      Console.WriteLine("   Vehicle Year: " + _year);
+      Console.WriteLine("   Vehicle Horsepower: " + _hp);
+      Console.WriteLine("   Vehicle Status: " + _status);
+    }
+
+    public void Wreck()
+    {
+      Console.WriteLine(_name + " Wrecks!");
+      _status = "wrecked";
+    }
+    #endregion
+
+  }
+}
---
+++ OOP/ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,50 @@
+﻿using ClassesAndObjects;
+
+
+namespace ClassesAndObjects
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Car audi = new Car("Audi", 75);
+      Car bmw = new Car("BMW", 350);
+      audi.PrintStats();
+      bmw.PrintStats();
+      audi.Drive();
+      bmw.Drive();
+
+
+
+      Console.WriteLine("Press 1 to stop audi.");
+      Console.WriteLine("Press 2 to stop bmw.");
+      string userInput = Console.ReadLine();
+
+      switch (userInput)
+      {
+        case "1":
+          audi.Stop(); break;
+        case "2":
+          bmw.Stop(); break;
+        default:
+          audi.Wreck();
+          bmw.Wreck();
+          break;
+      }
+      audi.PrintStats();
+      bmw.PrintStats();
+
+
+
+
+      Exit();
+
+    }
+
+    public static void Exit()
+    {
+      Console.WriteLine("Program will exit in 5 seconds...");
+      System.Threading.Thread.Sleep(5000);
+    }
+  }
+}
