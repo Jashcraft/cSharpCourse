0c425369bc4630e7862a6df08924ec7a4b8d7e3f
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-15 10:00:50 -0600

    finish loops section

---
+++ Making Decisions/Loops/Loops/BreakAndContinue/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace BreakAndContinue
+{
+	class Program
+	{
+		static void Main(string[] args)
+    {
+      Console.WriteLine("For Loop with Break: ");
+      breakLoop();
+
+      Console.WriteLine("For Loop With Continue");
+      continueLoop();
+
+
+      Console.WriteLine("Program will exit in 5 seconds ...");
+      System.Threading.Thread.Sleep(5000);
+    } 
+
+		public static void breakLoop ()
+		{
+      for (int i = 0; i < 10; i++)
+      {
+        Console.WriteLine(i);
+        if (i == 3)
+        {
+          Console.WriteLine("We stop at 3");
+          break;
+        }
+      }
+    }
+
+		public static void continueLoop () {
+      for (int i = 0; i < 10; i++)
+      {
+
+        if (i % 2 == 0)
+        {
+          Console.WriteLine("This number is even.");
+          continue;
+        }
+        Console.WriteLine(i);
+      }
+    }
+	}
+}
---
+++ Making Decisions/Loops/Loops/Loops/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Loops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			basicForLoop();
+			doWhileLoop();
+			calcStringLength();
+
+
+
+
+			Console.WriteLine("Program will exit in 5 seconds ...");
+			System.Threading.Thread.Sleep(5000);
+		}
+
+		public static void basicForLoop()
+		{
+
+			Console.WriteLine("Basic For Loop");
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(i);
+			}
+		}
+
+		public static void doWhileLoop()
+		{
+			Console.WriteLine("Do While Loop");
+			int counter = 0;
+			do
+			{
+				// This will run once at least 
+				Console.WriteLine(counter);
+				counter++;
+
+				//make sure these loops do END at some point.
+			} while (counter < 10);
+		}
+
+		public static void calcStringLength()
+		{
+			int lengthOfString = 0;
+			string wholeText = "";
+
+			do
+			{
+				Console.WriteLine("Write the name of a friend:");
+				string name = Console.ReadLine();
+				int currentLength = name.Length;
+				lengthOfString += currentLength;
+				wholeText += name;
+			} while (lengthOfString < 20);
+
+		}
+
+	}
+}
