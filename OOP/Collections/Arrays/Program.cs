b2bf5042cd121a8eec780be7514c429e55b6eada
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-16 12:28:10 -0600

    add arrays proj

---
+++ OOP/Collections/Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Arrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //declare and initialize
+      int[] grades = new int[5];
+      grades[0] = 20;
+      grades[1] = 15;
+      grades[2] = 30;
+      grades[3] = 40;
+      grades[4] = 15;
+
+      int[] gradesOfStudentsA = { 20, 22, 76, 15, 30 };
+      int[] gradesOfStudentsB = new int[] { 20, 22, 33 };
+
+      Console.WriteLine("length of gradesOfStudentsA array is: {0}", gradesOfStudentsA.Length);
+
+      SetGrades();
+
+      Exit();
+    }
+    public static void Exit()
+    {
+      Console.WriteLine("Program will exit in 4 seconds...");
+      System.Threading.Thread.Sleep(4000);
+    }
+
+    public static void SetGrades()
+    {
+      Console.WriteLine("Choose a student to update(1-5): ");
+      string indexInput = Console.ReadLine();
+      Console.WriteLine("Set grade for student(0-100): ");
+      string gradeInput = Console.ReadLine();
+
+      int selectedStudent = int.Parse(indexInput) - 1;
+      int grade = int.Parse(gradeInput);
+
+
+      Console.WriteLine("Student {0} now has a grade of {1}", selectedStudent, grade);
+    }
+  }
+}
---
+++ OOP/Collections/Arrays/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+  internal class Student
+  {
+    #region Private Student fields
+    //ToDo
+    private int _id;
+    private string _name; 
+    private string _email; 
+    private string _phone;
+    private int _age;
+    private string _gender;
+
+    #endregion
+
+    #region Public Student Fields
+    //ToDo
+    public string[] classes;
+    #endregion
+
+    #region Private Student Methods
+    //ToDo
+    #endregion
+
+    #region Public Student Methods
+    //ToDo
+    #endregion
+
+    #region Getters / Setters
+    //ToDo
+    public void GetStudentInfo()
+    {
+
+    }
+    #endregion
+
+    #region Constructors
+    //ToDo
+    #endregion
+
+  }
+}
