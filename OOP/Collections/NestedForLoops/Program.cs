d2760c3fc200beb7a66ca61c32de1bcf37ab92cb
Jashcraft <ashcraft.jonathon@gmail.com>
2023-08-19 21:56:52 -0600

    Minor Changes

---
+++ OOP/Collections/NestedForLoops/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+class Program
+{
+  static int[,] matrix =
+{
+  {1,2,3 },
+  {4,5,6},
+  {7,8,9 }
+};
+
+  static void Main()
+  {
+    foreach (var item in matrix)
+    {
+      Console.Write(item + " ");
+    }
+
+
+    Console.WriteLine("This is our 2D array printed using a nested for loop");
+    //outer for loop
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      //inner for loop
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+        int value = matrix[i, j];
+        if (value % 2 == 0)
+        {
+          Console.WriteLine(matrix[i, j] + " ");
+        }
+
+      }
+    }
+
+    Exit();
+  }
+  public static void Exit()
+  {
+    Console.WriteLine("\nProgram will exit in 4 seconds...");
+    System.Threading.Thread.Sleep(4000);
+  }
+
+}
---
+++ OOP/Collections/TicTacToe/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+
+  static char[,] fieldData =
+  {
+      {'1', '2', '3' },
+      {'4', '5', '6' },
+      {'7', '8', '9' }
+  };
+
+  static void Main()
+  {
+
+
+    DrawField();
+    Exit();
+  }
+
+/// <summary>
+/// Field Drawing Section
+/// </summary>
+  public static void DrawField()
+  {
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}  ", fieldData[0,0], fieldData[0,1], fieldData[0,2]);
+    Console.WriteLine("_____|_____|_____");
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}  ", fieldData[1, 0], fieldData[1, 1], fieldData[1, 2]);
+    Console.WriteLine("_____|_____|_____");
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}  ", fieldData[2, 0], fieldData[2, 1], fieldData[2, 2]);
+    Console.WriteLine("     |     |     ");
+
+  }
+  public static void Exit()
+  {
+    Console.WriteLine("\nProgram will exit in 4 seconds...");
+    System.Threading.Thread.Sleep(4000);
+  }
+
+}
